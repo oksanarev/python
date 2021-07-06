de5707f5b80bd1f475f80bdc54a388dc67dd69ea
oksanarev <86969159+oksanarev@users.noreply.github.com>
2021-07-06 15:35:37 +0300

    exchange_task7

--- lesson4_dz.py/task7.py
+++ lesson4_dz.py/task7.py
@@ -1,6 +1,10 @@
 # 7 задание.
+<<<<<<< HEAD
 from functools import reduce
 from itertools import count
+=======
+
+>>>>>>> b50c218 (exchange_task7)
 def fact (n):
     for i in range(1, n+1):
         yield i
@@ -13,8 +17,11 @@ for el in fact(n):
     f = f * el
     print(f'{el}!= {f}')
 
+<<<<<<< HEAD
 
 
 
 
 
+=======
+>>>>>>> b50c218 (exchange_task7)
